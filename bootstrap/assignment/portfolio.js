346ecb6233ed862378989d5ac5cc5d93e4636e80
priyadabhi <priyadabhi43@gmail.com>
2025-02-10 18:01:07 +0530

    portfolio

--- bootstrap/assignment/portfolio.js
+++ bootstrap/assignment/portfolio.js
@@ -1,12 +1,20 @@
-function changeicon(icon) {
-    let i = document.getElementById(icon)
-    document.body.classList.toggle("menu")
 
-    if (document.body.classList.contains("menu")) {
+
+function changeicon(icon, menu) {
+    let i = document.getElementById(icon)
+    let m = document.getElementById(menu)
+    m.classList.toggle("menu")
+
+    if (m.classList.contains("menu")) {
         i.classList.remove("fa-bar")
         i.classList.add("fa-xmark")
+        m.style.display = "flex"
+        m.style.flexDirection = "column"
+        m.style.flexWrap = "nowrap"
     } else {
         i.classList.remove("fa-xmark")
         i.classList.add("fa-bar")
+        m.style.display = "none"
+        // menu.style.display = "none"
     }
 }
